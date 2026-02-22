326c484ed43d663eefea74a637f709544ce0694d
andreaste97-max <andreaste97@gmail.com>
2026-02-22 10:24:37 +0100

    Fix: timer digiuno persiste tra tab/sessioni + force cache invalidation per PC

--- sw.js
+++ sw.js
@@ -1,17 +1,16 @@
-var CACHE_NAME='pcos-daily-v2';
+var CACHE_NAME='pcos-daily-v3';
 var urlsToCache=['/','./index.html'];
 self.addEventListener('install',function(event){
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then(function(cache){return cache.addAll(urlsToCache);}));
 });
 self.addEventListener('fetch',function(event){
-  event.respondWith(caches.match(event.request).then(function(response){
-    return response||fetch(event.request).then(function(res){
-      return caches.open(CACHE_NAME).then(function(cache){cache.put(event.request,res.clone());return res;});
-    });
-  }));
+  event.respondWith(fetch(event.request).then(function(res){
+    return caches.open(CACHE_NAME).then(function(cache){cache.put(event.request,res.clone());return res;});
+  }).catch(function(){return caches.match(event.request);}));
 });
 self.addEventListener('activate',function(event){
   event.waitUntil(caches.keys().then(function(names){
     return Promise.all(names.filter(function(n){return n!==CACHE_NAME;}).map(function(n){return caches.delete(n);}));
-  }));
+  }).then(function(){return self.clients.claim();}));
 });
